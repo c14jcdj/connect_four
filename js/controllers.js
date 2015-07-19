3afe37a3dbf6a10d5faf2ce212af2bbbad978ee1
Chermaine Zimmerman <C14jcdj@yahoo.com>
2015-07-18 19:23:13 -0700

    Refactored Code

--- js/controllers.js
+++ js/controllers.js
@@ -7,57 +7,24 @@ var connectFourControllers = angular.module('connectFourControllers', []);
 
 
 
-connectFourControllers.controller('gameController', [ 'boardFactory', 'pieceFactory', 'playerFactory',
-  function(boardFactory, pieceFactory, playerFactory) {
-    var _this = this;
-    var board = new boardFactory.Board(7,6);
-    var player1 = new playerFactory.Player('player1', 'blue', true)
-    var player2 = new playerFactory.Player('player2', 'red', false)
-
-    var turn = 1;
+connectFourControllers.controller('gameController', [ 'boardFactory', 'pieceFactory', 'playerFactory', 'gameFactory',
+  function(boardFactory, pieceFactory, playerFactory, gameFactory) {
+    var _this = this,
+        game = new gameFactory.Game(),
+        board = new boardFactory.Board(7,6),
+        player1 = new playerFactory.Player('player1', 'blue', true),
+        player2 = new playerFactory.Player('player2', 'red', false),
+        turn = 1;
     
-    
-    
-    _this.board = board.gameBoard;
-
-    var checkForWinner = function (emptySlot) {
-      var row = emptySlot[0]
-      var column = emptySlot[1]
-      if(board.checkFourAcross(row) ){
-        alert('winner');
-        return;
-      }
-      if(board.checkFourDown(column)){
-        alert('winner');
-        return;
-      }
-      if(board.checkFourDiagonal(emptySlot)){
-        alert('winner');
-        return;
-      }
-      
+    _this.gameBoard = board.gameBoard;
+    _this.board = board;
+    _this.player1 = player1;
+    _this.player2 = player2;
+    _this.turn = turn;
+    _this.pieceFactory = pieceFactory;
+    _this.columnSelected = function(ctrl,column){
+      game.columnSelected(ctrl,column);
     }
-    _this.selectColumn = function(column){
 
-      var emptySlot = board.checkColumnForEmptySlots(column)
-      function isOdd(num) { return num % 2;}
-      var player = isOdd(turn) ? player1 : player2
-      var piece = new pieceFactory.Piece(player)
-      if(emptySlot) {
-        board.insertGamePiece(piece, emptySlot)
-        turn += 1;
-
-      } else {
-         _this.message = 'Column Full! Try another column'
-      }  
-
-      if(turn >7){
-        checkForWinner(emptySlot)
-      }
-
-      
-    } 
-
-    board.selectColumn;
     board.init();
   }]);
--- js/services.js
+++ js/services.js
@@ -1,221 +1,280 @@
 var connectFourServices = angular.module('connectFourApp');
+
 connectFourServices.factory('boardFactory', ['$http', function($http) {
     
-    function Board(columns, rows){
-      this.columns = columns;
-      this.rows = rows;
-      this.gameBoard = [];
+  function Board(columns, rows){
+    this.columns = columns;
+    this.rows = rows;
+    this.gameBoard = [];
 
-      this.init = function () {
+    this.init = function () {
 
-        //Create new board with empty slots
-        for(var i=0, ii = this.rows; i < ii ; i++){
-          var array = new Array(7);
-          this.gameBoard.push(array)
-        }
-
-      }
-
-      this.isSlotEmpty = function (column, row) {
-        return this.gameBoard[row][column] == undefined ? true : false;
-      }
-
-      this.checkColumnForEmptySlots = function (column) {
-
-        var rows = this.rows;
-        var emptySlot;
-
-        //Check column from bottom up if there is an empty slot
-        for(var row=this.rows -1; row >= 0; row--){
-          if(this.isSlotEmpty(column, row)){
-            emptySlot = [row, column]
-            break;
-          }
-        }
-
-        return emptySlot;
-          
-      }
-
-      this.insertGamePiece = function (piece, emptySlot) {
-
-        var row = emptySlot[0],
-            column = emptySlot[1];
-        
-        this.gameBoard[row][column] = piece;
-
-      }
-
-      this.checkFourAcross = function  (row) {
-        var row = this.gameBoard[row]
-        var color = row[0] ? row[0].color : 'white';
-        var piecesInARow = 1;
-        for(var i = 1, ii = row.length; i < ii; i++){
-          if(row[i] != undefined){
-            if(row[i].color == color){
-              piecesInARow +=1;
-            } else {
-              color = row[i].color
-              piecesInARow = 1;
-            } 
-          } else {
-            color = 'white'
-          }
-          
-        }
-          return piecesInARow >= 4;
-        
-      }
-      this.checkFourDown = function  (column) {
-
-        var color = this.gameBoard[0][column] ? this.gameBoard[0][column].color : 'white';
-        var piecesInARow = 1;
-        for(var i = 1, ii=this.rows; i<ii; i++){
-          if(this.gameBoard[i][column] != undefined){
-            if(this.gameBoard[i][column].color == color){
-              piecesInARow +=1
-            }else {
-              color = this.gameBoard[i][column].color
-              piecesInARow = 1;
-            }
-          } else {
-            color = 'white';
-          }
-
-        }
-
-        return piecesInARow >= 4;
-      }
-      this.checkFourDiagonal = function  (coordinates) {
-        var rows = this.rows;
-        var columns = this.columns;
-        var gameBoard = this.gameBoard;
-        function findBaseCoordinatesLeftDiagonal(){
-          var row = coordinates[0];
-          var column = coordinates[1];
-          while(row < rows -1 && column > 0){
-            row += 1;
-            column -=1;
-          }
-          return [row, column]
-        }
-
-        function findBaseCoordinatesRightDiagonal(){
-          var row = coordinates[0];
-          var column = coordinates[1];
-          while(row < rows-1 && column < columns-1){
-            row += 1;
-            column +=1;
-          }
-          return [row, column]
-        }
-
-        function checkFourInARow (coordinates, direction) {
-          console.log(coordinates)
-          var row = coordinates[0];
-          var column = coordinates[1];
-          var color = gameBoard[row][column] ? gameBoard[row][column].color : 'white';
-          var piecesInARow = 1;
-          
-          if(direction == 'right'){
-            row -= 1;
-            column += 1; 
-
-            while(row - 1 >= 0 && column + 1 < columns ){
-              if(gameBoard[row][column] != undefined){
-              console.log(gameBoard[row][column].color)
-                if(gameBoard[row][column].color == color){
-                  piecesInARow +=1
-                }else {
-                  color = gameBoard[row][column].color
-                  piecesInARow = 1;
-                }
-              } else {
-                color = 'white'
-              }
-
-              row -= 1;
-              column += 1;
-              
-              
-            }
-              
-              return piecesInARow >= 4;
-          } else {
-            row -= 1;
-            column -= 1; 
-
-            while(row - 1 >= 0 && column >= 0 ){
-              console.log('row '+row+' column '+column+'')
-              console.log('control color ' +color)
-              if(gameBoard[row][column] != undefined){
-              console.log(gameBoard[row][column].color)
-                if(gameBoard[row][column].color == color){
-                  console.log('yes')
-                  piecesInARow +=1
-                }else {
-                  console.log('no')
-                  color = gameBoard[row][column].color
-                  piecesInARow = 1;
-                }
-              } else {
-                color = 'white'
-              }
-
-              row -= 1;
-              column -= 1;
-              console.log('pieces in a row ' + piecesInARow)
-              
-            }
-              return piecesInARow >= 4;
-          }
-          
-
-        }
-        
-        var coordinatesLeftDiagonal = findBaseCoordinatesLeftDiagonal()
-        var coordinatesRightDiagonal = findBaseCoordinatesRightDiagonal()
-        if(checkFourInARow(coordinatesLeftDiagonal, 'right')){
-          return true
-        }
-        if(checkFourInARow(coordinatesRightDiagonal, 'left')){
-          return true
-        }
-
+      //Create new board with empty slots
+      for(var i=0, ii = this.rows; i < ii ; i++){
+        this.gameBoard.push(new Array(7))
       }
 
     }
 
+    this.isSlotEmpty = function (column, row) {
+      return this.gameBoard[row][column] == undefined ? true : false;
+    }
 
-   return {
-    Board:Board
-   }
-}]);
+    this.checkColumnForEmptySlots = function (column) {
+      var emptySlot;
 
-connectFourServices.factory('pieceFactory', ['$http', function($http) {
-
-      function Piece (player) {
-        this.player = player;
-        this.color = player.color;
+      //Check column from bottom up if there is an empty slot
+      for(var row=this.rows -1; row >= 0; row--){
+        if(this.isSlotEmpty(column, row)){
+          emptySlot = [row, column]
+          break;
+        }
       }
-    
-       return {
-    Piece:Piece
-   }
+
+      return emptySlot;
+        
+    }
+
+    this.insertGamePiece = function (piece, emptySlot) {
+      var row = emptySlot[0],
+          column = emptySlot[1];
+      
+      this.gameBoard[row][column] = piece;
+    }
+
+    this.checkFourAcross = function (row) {
+      var row = this.gameBoard[row],
+          color = row[0] ? row[0].color : 'white',
+          variableObj = {
+            'iterator' : row,
+            'maxLength' : row.length,
+            'color' : color,
+            'piecesInARow' : 1,
+            'checkType' : 'linear'
+          };
+      
+      return checkHowManySameColoredPiecesInARow(variableObj).piecesInARow >= 4;
+    }
+
+    this.checkFourDown = function  (column) {
+      var maxLength = this.rows,
+          color = this.gameBoard[0][column] ? this.gameBoard[0][column].color : 'white',
+          gameBoard = this.gameBoard,
+          variableObj = {
+            'iterator' : gameBoard,
+            'maxLength' : maxLength,
+            'color' : color,
+            'piecesInARow' : 1,
+            'option' : column,
+            'checkType' : 'linear'
+          };
+      
+      return checkHowManySameColoredPiecesInARow(variableObj).piecesInARow >= 4;
+    }
+
+    this.checkFourDiagonal = function  (coordinates) {
+      var rows = this.rows;
+          columns = this.columns,
+          row = coordinates[0],
+          column = coordinates[1],
+          gameBoard = this.gameBoard,
+          baseCoordinatesLeftDiagonal = undefined, 
+          baseCoordinatesRightDiagonal = undefined,
+          findBaseCoordinates = function (diagonalType){
+            var row = coordinates[0],
+                column = coordinates[1],
+                columnBoundary = diagonalType == 'left' ? (column > 0) : (column < columns -1); 
+            
+            while(row < rows - 1 && columnBoundary){
+              row += 1;
+              diagonalType == 'left'? column -=1 : column += 1;
+            }
+
+            return [row, column]
+          },
+          checkForFourPiecesInARow = function (coordinates, direction) {
+            var row = coordinates[0],
+                column = coordinates[1],
+                color = gameBoard[row][column] ? gameBoard[row][column].color : 'white',
+                variableObj = {
+                  'color' : color,
+                  'piecesInARow' : 1,
+                  'checkType' : 'diagonal',
+                  'iterator': gameBoard[row][column],
+                  'row' : row,
+                  'column' : column,
+                  'columns' : columns,
+                  'gameBoard' : gameBoard
+                };
+
+            if(direction == 'right'){
+              variableObj.direction = 'right'
+              return checkHowManySameColoredPiecesInARow(variableObj).piecesInARow >= 4;
+            } else {
+              variableObj.direction = 'left'
+              return checkHowManySameColoredPiecesInARow(variableObj).piecesInARow >= 4;
+            }
+          };
+      
+      //Set base coordinates to start diagonal search
+      baseCoordinatesLeftDiagonal = findBaseCoordinates('left'),
+      baseCoordinatesRightDiagonal = findBaseCoordinates('right');
+      
+      //Check for four in a row starting from base coordinates
+      if(checkForFourPiecesInARow(baseCoordinatesLeftDiagonal, 'right')){ return true };
+      if(checkForFourPiecesInARow(baseCoordinatesRightDiagonal, 'left')){ return true };
+    }
+  }
+
+  //Private
+
+  function checkHowManySameColoredPiecesInARow(obj){
+      var iterator = obj.iterator,
+          color = obj.color,
+          piecesInARow = obj.piecesInARow,
+          maxLength = obj.maxLength,
+          resetControlToNewColor = function (piece){
+            color = piece.color
+            piecesInARow = 1;
+          },
+          incrementPiecesInARowVar = function (){
+            piecesInARow +=1;
+          },
+          checkColor = function (piece){
+            //If piece color is equal to control color increment var, if not reset var and color
+            piece.color && piece.color == color ? incrementPiecesInARowVar() : resetControlToNewColor(piece);
+          },
+          resetColorToDefault = function (){
+            color = 'white';
+          },
+          updateObj = function (){
+            obj.piecesInARow = piecesInARow;
+          },
+          linearCheck = function (){
+            for(var i = 1, ii = maxLength; i < ii; i++){
+              if(iterator[i]){
+                //Determine iterator to use: check if there is an option on object(column vs row)
+                var iteratorToUse = obj.option != undefined ? iterator[i][obj.option] : iterator[i]
+
+                //If there is a piece, check for color; if not reset color back to default
+                iteratorToUse ? checkColor(iteratorToUse) : resetColorToDefault()
+              }
+            }
+          },
+          diagonalCheck = function (){
+            var row = obj.row,
+                column = obj.column,
+                gameBoard = obj.gameBoard,
+                direction = obj.direction,
+                columns = obj.columns,
+                columnBoundary,
+                incrementDiagonal = function (direction){
+                  row -= 1;
+                  direction == 'right' ? column += 1 : column -= 1;
+
+                  //Sets max column or min column depending on direction
+                  columnBoundary = direction == 'right' ? (column + 1 < columns) : column >=0;
+                }
+
+            //Increment to next spot to start checking color since control color is set to base coordinate color
+            direction == 'right' ? incrementDiagonal('right') : incrementDiagonal('left')
+
+            while(row - 1 >= 0 && columnBoundary ){
+              //If there is a piece, check for color; if not reset color back to default
+              gameBoard[row][column] ? checkColor(gameBoard[row][column]) : resetColorToDefault()
+              direction == 'right' ? incrementDiagonal('right') : incrementDiagonal('left')
+            }
+
+          };
+      
+      obj.checkType == 'linear' ? linearCheck() : diagonalCheck();
+
+      updateObj()        
+
+      return obj
+    }
+
+ return {
+  Board:Board
+ }
 }]);
 
-connectFourServices.factory('playerFactory', ['$http', function($http) {
+connectFourServices.factory('pieceFactory', [ function() {
 
-      function Player (name, color, first) {
-        this.name = name;
-        this.color = color;
-        this.first = first;
+  function Piece (player) {
+    this.player = player;
+    this.color = player.color;
+  }
+  
+  return {
+    Piece:Piece
+  }
+
+}]);
+
+connectFourServices.factory('playerFactory', [ function() {
+
+  function Player (name, color, first) {
+    this.name = name;
+    this.color = color;
+    this.first = first;
+  }
+  
+  return {
+    Player:Player
+  }
+
+}]);
+
+connectFourServices.factory('gameFactory', [ function() {
+
+  function Game () {
+
+    this.checkForWinner = function (board,emptySlot) {
+      var row = emptySlot[0],
+          column = emptySlot[1];
+
+      if(board.checkFourAcross(row) ){
+        alert('winner across ' + row);
+        return;
+      }
+      if(board.checkFourDown(column)){
+        alert('winner down' + column);
+        return;
+      }
+      if(board.checkFourDiagonal(emptySlot)){
+        alert('winner dia' + emptySlot );
+        return;
+      }
+    }
+
+    this.isOdd = function (num) {
+      return num % 2;
+    }
+
+    this.columnSelected = function(ctrl,column){
+      var emptySlot = ctrl.board.checkColumnForEmptySlots(column),
+          player = this.isOdd(ctrl.turn) ? ctrl.player1 : ctrl.player2,
+          piece = new ctrl.pieceFactory.Piece(player);
+          addGamePiece = function(){
+            ctrl.board.insertGamePiece(piece, emptySlot)
+            ctrl.turn += 1;
+          },
+          displayErrorMessage = function(){
+            ctrl.message = 'Column Full! Try another column'   
+          };
 
         
-      }
-    
-       return {
-    Player:Player
-   }
+      emptySlot ? addGamePiece() : displayErrorMessage()
+
+      // Start checking for winner after turn 7
+      if(ctrl.turn >7){ this.checkForWinner(ctrl.board,emptySlot) }
+    }
+  }
+  
+  return {
+    Game:Game
+  }
+
 }]);
 
